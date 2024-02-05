fd69a8fc6c3f2dc704b6600aef624b41bd850644
吃瓜群众 <myt2000@126.com>
2024-02-05 11:08:17 +0800

    搜索插入位置

--- 35.搜索插入位置.js
+++ 35.搜索插入位置.js
@@ -6,9 +6,9 @@
 var searchInsert = function (nums, target) {
     let left = 0;
     let right = nums.length - 1;
-    while (left < right) {
+    while (left <= right) {
         mid = Math.floor((left + right) / 2);
-        if (nums[mid] == target)
+        if (nums[mid] === target)
             return mid;
         else if (nums[mid] < target)
             left = mid + 1;
