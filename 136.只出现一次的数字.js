3a4e1e3e31d8767250bc525167db3c99a85a47a2
吃瓜群众 <myt2000@126.com>
2024-03-04 17:31:26 +0800

    只出现一次的数字

--- 136.只出现一次的数字.js
+++ 136.只出现一次的数字.js
@@ -23,6 +23,8 @@ var singleNumber = function (nums) {
     }
     return single
 };
-console.log(singleNumber([2, 2, 2, 1]))
+
+
+console.log(singleNumber([2, 2, 2, 3]))
 console.log(singleNumber([4, 1, 2, 1, 2]))
 console.log(singleNumber([1]))
