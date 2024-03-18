c414e9125098d07cb7629f5af5a37892351003a3
吃瓜群众 <myt2000@126.com>
2024-03-18 14:35:14 +0800

    用队列实现栈二次实现

--- 225.用队列实现栈.js
+++ 225.用队列实现栈.js
@@ -1,61 +1,45 @@
 
-var MyStack = function () {
-    this.queue1 = [];
-    this.queue2 = [];
-};
+class MyStack {
+    constructor() {
+        this.queue1 = [];
+        this.queue2 = [];
+    }
 
-/** 
- * @param {number} x
- * @return {void}
- */
-MyStack.prototype.push = function (x) {
-    // 将元素添加到queue1的末尾
-    this.queue1.push(x);
-    // 将queue2的元素移动到queue1, 以保持queue2为空
-    while (this.queue2.length > 0 && this.queue1.length > 1) {
-        if (this.queue1.length > 0) {
-            let temp = this.queue1.shift()
-            this.queue2.push(temp);
+    push(x) {
+        // 将元素添加到 queue1 的末尾
+        this.queue1.push(x);
+        // 确保 queue2 为空，然后将 queue1 中的所有元素移动到 queue2，并反转顺序
+        while (this.queue2.length > 0 || this.queue1.length > 1) {
+            if (this.queue1.length > 0) {
+                this.queue2.push(this.queue1.shift());
+            }
         }
     }
-};
 
-/**
- * @return {number}
- */
-MyStack.prototype.pop = function () {
-    // 如果queue2 为空，则将 queue1 中的元素全部移到 queue2 , 并反转顺序
-    if (this.queue1.length === 0) {
-        while (this.queue2.length > 0) {
-            this.queue1.push(this.queue2.shift());
+    pop() {
+        // 如果 queue1 为空，则将 queue2 中的元素全部移到 queue1，并返回并移除 queue1 的第一个元素（即栈顶元素）
+        if (this.queue1.length === 0) {
+            while (this.queue2.length > 0) {
+                this.queue1.push(this.queue2.shift());
+            }
         }
+        return this.queue1.shift();
     }
-    // 返回并移除queue2的第一个元素（即栈顶元素）
-    return this.queue1.shift();
 
-};
-
-/**
- * @return {number}
- */
-MyStack.prototype.top = function () {
-    // 同 top 方法，确保queue2 有元素可返回
-    if (this.queue1.length === 0) {
-        while (this.queue2.length > 0) {
-            this.queue1.push(this.queue2.shift())
+    top() {
+        // 同 pop 方法，确保栈顶元素位于 queue1
+        if (this.queue1.length === 0) {
+            while (this.queue2.length > 0) {
+                this.queue1.push(this.queue2.shift());
+            }
         }
+        return this.queue1[0];
     }
-    // 返回queue2 的第一个元素
-    return this.queue1[0]
-};
 
-/**
- * @return {boolean}
- */
-MyStack.prototype.empty = function () {
-    // 检查两个队列是否都为空
-    return this.queue1.length === 0 && this.queue2.length === 0;
-};
+    empty() {
+        return this.queue1.length === 0 && this.queue2.length === 0;
+    }
+}
 
 /**
  * Your MyStack object will be instantiated and called as such:
