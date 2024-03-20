32dcde3b00a400915c03ac90c8b87c660dd01b89
吃瓜群众 <myt2000@126.com>
2024-03-20 16:37:26 +0800

    有效的字母异位词 最优解

--- simple/242.有效的字母异位词.js
+++ simple/242.有效的字母异位词.js
@@ -3,11 +3,12 @@
  * @param {string} t
  * @return {boolean}
  */
+// 思路：构建一个map 字典结构，先遍历s， 统计字母出现的次数，然后遍历t，对每次出现的字母一次相同是否相同，相同对应的值就减1，最后判断map是否值都为0，如果都为0，返回true，否则返回false
 var isAnagram = function (s, t) {
     if (s.length !== t.length) {
         return false;
     }
-
+    // map和object的区别
     const countMap = new Map();
 
     for (const char of s) {
@@ -41,3 +42,33 @@ console.log(isAnagram("rat", "car")); // 输出：false
 
 // 示例测试（假设字符串只包含 Unicode 小写字母）
 console.log(isAnagram("ānagrām", "nagrāmā")); // 输出：true
+
+
+// 最优解, 生成一个26长度的数组，
+
+var isAnagramBest = function (s, t) {
+    if (s.length !== t.length) {
+        return false;
+    }
+    const base = 'a'.charCodeAt(0);
+    const alphabetArr = new Array(26).fill(0);
+    for (const alphabet of s) {
+        const idx = alphabet.charCodeAt(0) - base;
+        // 这里是自增操作，可以直接赋值到数组
+        alphabetArr[idx]++;
+    }
+    for (const alphabet of t) {
+        const idx = alphabet.charCodeAt(0) - base;
+        if (!alphabetArr[idx]) {
+            return false;
+        } else {
+            alphabetArr[idx]--;
+        }
+    }
+
+    return true;
+};
+
+// 示例测试
+console.log(isAnagramBest("anagram", "nagaram")); // 输出：true
+console.log(isAnagramBest("rat", "car")); // 输出：false
