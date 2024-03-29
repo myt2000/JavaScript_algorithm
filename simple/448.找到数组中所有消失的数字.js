2fc5b55ee62c2c5a26abf719d159302f66b183ed
吃瓜群众 <myt2000@126.com>
2024-03-29 10:55:51 +0800

    找到数组中所有消失的数字  leetcode最优解

--- simple/448.找到数组中所有消失的数字.js
+++ simple/448.找到数组中所有消失的数字.js
@@ -24,3 +24,22 @@ var findDisappearedNumbers = function (nums) {
 // 示例用法
 console.log(findDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 1])); // 输出 [5,6]
 console.log(findDisappearedNumbers([1, 1])); // 输出 [2]
+
+// leetcode最佳解法，通过设置为
+var findDisappearedNumbersBest = function (nums) {
+    const tmp = new Array(nums.length);
+    const res = [];
+    nums.forEach(num => {
+        tmp[num - 1] = num;
+    })
+    for (let i = 0; i < tmp.length; i++) {
+        if (tmp[i] === undefined) res.push(i + 1);
+    }
+    return res;
+};
+
+/**
+ * const tmp = new Array(nums.length);：创建一个与 nums 数组相同长度的临时数组 tmp，初始值为 undefined。
+ * nums.forEach(num => { tmp[num - 1] = num; })：遍历 nums 数组，将每个数字 num 对应的索引 num - 1 处的值设为 num，表示该数字已经出现过。
+ * for (let i = 0; i < tmp.length; i++) { if (tmp[i] === undefined) res.push(i + 1); }：再次遍历 tmp 数组，找出所有值为 undefined 的索引，将这些索引加一后加入到结果数组 res 中，表示这些数字是未出现过的。
+*/
