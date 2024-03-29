8956938681b2f7976569339f835265db1ca246f2
吃瓜群众 <myt2000@126.com>
2024-03-29 10:43:58 +0800

    找到数组中所有消失的数字

---
+++ simple/448.找到数组中所有消失的数字.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+// 索引关系和数组值的关系，通过数组元素值，确定元素位置，然后将非负数的元素位置，加入结果数组中
+var findDisappearedNumbers = function (nums) {
+    for (let i = 0; i < nums.length; i++) {
+        let index = Math.abs(nums[i]) - 1;
+        if (nums[index] > 0) {
+            nums[index] *= -1;
+        }
+    }
+
+    // 再次遍历数组，查找没有找到的数字
+    const result = [];
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] > 0) {
+            result.push(i + 1);
+        }
+    }
+    return result
+};
+
+// 示例用法
+console.log(findDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 1])); // 输出 [5,6]
+console.log(findDisappearedNumbers([1, 1])); // 输出 [2]
