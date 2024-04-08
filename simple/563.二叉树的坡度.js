ff63ae063fec403b1cad7e5c3b9917225c9bfb7f
吃瓜群众 <myt2000@126.com>
2024-04-08 17:46:30 +0800

    二叉树的坡度

---
+++ simple/563.二叉树的坡度.js
@@ -0,0 +1,77 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+class TreeNode {
+    constructor(val, left, right) {
+        this.val = (val === undefined ? 0 : val)
+        this.left = (left === undefined ? null : left)
+        this.right = (right === undefined ? null : right)
+    }
+}
+
+
+var findTilt = function (root) {
+    let res = 0;
+    function dfs(node) {
+        if (!node) return 0;
+        let left = dfs(node.left)
+        let right = dfs(node.right)
+        res += Math.abs(left - right);
+        return node.val + left + right;
+    }
+    dfs(root)
+    return res;
+};
+
+
+
+
+// 构建二叉树的辅助函数, 这个可以剔除中间有null的数组, 优化了代码value !== null
+function buildTree(nums) {
+    if (!nums.length) return null;
+    // 创建根节点
+    let root = new TreeNode(nums.shift());
+    let queue = [root]; // 最多有两个节点，左右节点
+    // 循环队列，直到队列为空或者数组为空
+    while (queue.length && nums.length) {
+        let node = queue.shift();
+        if (nums.length) {
+            const value = nums.shift();
+            if (value !== null) {
+                node.left = new TreeNode(value);
+                // 左节点放入queue
+                queue.push(node.left);
+            }
+        }
+        if (nums.length) {
+            const value = nums.shift();
+            if (value !== null) {
+                node.right = new TreeNode(value);
+                // 右节点放入queue
+                queue.push(node.right);
+            }
+
+        }
+    }
+    return root;
+}
+
+
+// 示例
+let array1 = [1, 2, 3];
+let root1 = buildTree(array1);
+console.log(findTilt(root1));
+
+
+let array2 = [4, 2, 9, 3, 5, null, 7];
+let root2 = buildTree(array2);
+console.log(findTilt(root2));
