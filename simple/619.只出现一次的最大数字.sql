45c694512f129c9b3db7dbd0f11be6fbed7399f2
吃瓜群众 <myt2000@126.com>
2024-04-10 15:52:31 +0800

    只出现一次的最大数字  最优解

--- simple/619.只出现一次的最大数字.sql
+++ simple/619.只出现一次的最大数字.sql
@@ -63,4 +63,21 @@ ORDER BY
     COUNT(num),
     num DESC
 LIMIT
-    1
+    1;
+
+# leetcode最优解
+select
+    max(num) as num
+from
+    mynumbers
+where
+    num in (
+        select
+            num
+        from
+            mynumbers
+        group by
+            num
+        having
+            count(num) = 1
+    );
