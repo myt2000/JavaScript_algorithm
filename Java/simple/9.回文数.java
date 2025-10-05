020850a5b9dc4f7894091f3b18740077f6b07a0f
work device <myt2000@126.com>
2025-10-05 21:35:58 +0800

    java 回文数

---
+++ Java/simple/9.回文数.java
@@ -0,0 +1,19 @@
+package Java.simple;
+class Solution9 {
+    public boolean isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+        int revertedNumber = 0;
+        while (x > revertedNumber) {
+            revertedNumber = revertedNumber * 10 + x % 10;
+            x /= 10;
+        }
+        return x == revertedNumber || x == revertedNumber / 10;
+    }
+
+    public static void main(String[] args) {
+        Solution9 solution = new Solution9();
+        System.out.println(solution.isPalindrome(121));
+    }
+}
